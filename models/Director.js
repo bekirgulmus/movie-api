beedc3996718b968a9e28191985d78c0f6b015f0
Bekir Gulmus <bekir.gulmus@hotmail.com>
2024-03-06 19:42:52 +0300

    DirectorSchema Validation added

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        maxLength: 60,
+        minLength: 2
+    },
+    surname: {
+        type: String,
+        maxLength: 60,
+        minLength: 2
+    },
+    bio: {
+        type: String,
+        maxLength: 1000,
+        minLength: 60
+    },
     createdAt: {
         type: Date,
         default: Date.now
